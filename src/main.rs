5d84926b953de8a041992050fb698698cdcbd3b9
David Harting <david.e.harting@gmail.com>
2021-06-02 19:28:40 -0400

    set up very basic clap app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use clap::{AppSettings, Clap};
+
+#[derive(Clap)]
+#[clap(version = "1.0", author = "David Harting <david.harting@hey.com>")]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Opts {
+  #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Clap)]
+enum SubCommand {
+    #[clap(version = "1.0")]
+    Rand(Rand)
+}
+
+#[derive(Clap)]
+struct Rand{
+    #[clap(short, long, default_value="10")]
+    length: u32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts : Opts = Opts::parse();
+
+    match opts.subcmd {
+        SubCommand::Rand(r) => {
+            println!("Ran 'rand' with length {}", r.length)
+        }
+    }
 }
