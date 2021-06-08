357dbdbb99a28fc74cc4ae7b2ad0919247ed4347
David Harting <david.e.harting@gmail.com>
2021-06-07 20:40:40 -0400

    Add help text to subcommands

--- src/main.rs
+++ src/main.rs
@@ -6,31 +6,37 @@ mod rand;
 #[clap(version = "1.0", author = "David Harting <david.harting@hey.com>")]
 #[clap(setting = AppSettings::ColoredHelp)]
 struct Opts {
-  #[clap(subcommand)]
+    #[clap(subcommand)]
     subcmd: SubCommand,
 }
 
 #[derive(Clap)]
 enum SubCommand {
-    #[clap(version = "1.0")]
-    Rand(Rand)
+    #[clap(about="Generate random values")]
+    Rand(Rand),
+    #[clap(about="Work with package.json files")]
+    Pj(Pj),
 }
 
 #[derive(Clap)]
-struct Rand{
-    #[clap(short, long, default_value="10")]
+struct Rand {
+    #[clap(short, long, default_value = "10")]
     length: usize,
 }
 
+#[derive(Clap)]
+struct Pj {}
+
 fn main() {
-    let opts : Opts = Opts::parse();
+    let opts: Opts = Opts::parse();
 
     match opts.subcmd {
         SubCommand::Rand(r) => {
-            let config  = rand::RandConfig {
-                length: r.length
-            };
+            let config = rand::RandConfig { length: r.length };
             println!("{}", rand::generate(&config));
+        }
+        SubCommand::Pj(_) => {
+            println!("Running pj");
         }
     }
 }
